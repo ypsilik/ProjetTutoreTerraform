a5dba7a987e64b9f2528561fa9971d025ec2171b
Maud Laurent <maud.laurent3@etu.univ-lorraine.fr>
2017-02-11 13:35:53 +0100

    sup doublons

---
+++ instanceNova.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from novaclient.v1_1 import client
+
+def get_nova_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
+
+nova = client.Client(**get_nova_creds())
+
